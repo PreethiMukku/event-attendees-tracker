afa6d47cc9c4fb6b1c89154474da51963968294f
Preethi_Mukku <Preethi_Mukku@EPINHYDW015C>
2020-06-22 17:57:54 +0530

    Add project files.

---
+++ Event-Attendees-Tracker_DAL/DBQueries/Roles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace Event_Attendees_Tracker_DAL.DBQueries
+{
+    public class Roles
+    {
+        static Event_Attendees_Tracker_DAL.Database_Context.EAT_DBContext _eatDBContext = Event_Attendees_Tracker_DAL.Instances.DBInstance.getDBInstance();
+
+        public static string FetchUserRoles(int UserID)
+        {
+            try
+            {
+                //TODO:
+                //When Drop down in UI will add make request based on the requested role
+                //&& role.Master_DBRoles.ID == roleID
+                var responseRoles = _eatDBContext.UserMappedRoles
+                    .Include(role => role.Master_DBRoles)
+                    .Where(role => role.UserDetails.ID == UserID)
+                    .ToList();
+
+                return responseRoles[0].Master_DBRoles.RoleName;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
